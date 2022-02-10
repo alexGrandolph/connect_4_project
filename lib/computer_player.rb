0ea663a8f86b7abea9351a1ebd741c7eee498386
alex randolph <alexgeorgerandolph@gmail.com>
2022-02-09 19:37:02 -0700

    Add cell.rb and cell_spec.rb

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -1,45 +1,41 @@
-board = [
-  ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
-  ['.', '.', '.', '.', '.', '.', '.'],
-  ['.', '.', '.', '.', '.', '.', '.'],
-  ['.', '.', '.', '.', '.', '.', '.'],
-  ['.', '.', '.', '.', '.', '.', '.'],
-  ['.', '.', '.', '.', '.', '.', '.'],
-  ['.', '.', '.', '.', '.', '.', '.'],
-]
+
 
 class Computer_player
   def initialize
-    @board = [
-      ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
-      ['.', '.', '.', '.', '.', '.', '.'],
-      ['.', '.', '.', '.', '.', '.', '.'],
-      ['.', '.', '.', '.', '.', '.', '.'],
-      ['.', '.', '.', '.', '.', '.', '.'],
-      ['.', '.', '.', '.', '.', '.', '.'],
-      ['.', '.', '.', '.', '.', '.', '.'],
-    ]
+  end
+  def make_board
+    @board = Array.new(6) { Array.new(7) { "."} }
+
+    print @board
+  end
+
+  def get_choice
+    @choice = rand(7)
+  end
+
+  def play_piece
+    @board
+    new = []
+    @board.each do |rows|
+      rows.each do |column|
+        new << column.contains?(".")
+      end
+    p new  
+    end
+
 
   end
 
-  def make_choice
-    @selection = 0
 
-  end
-
-  def place_piece
-    @selection = 0
-    if @board[0, 0].include?(".")
-
-    # if @selection == 0
-  #     @board[0,0].push('X')
-  #   end
-  #   print @board
+  # def play_piece(column)
+  #   get_choice
+  #   column = @choice
+  #   @board.map { |row| row|column }.any? {|slot| slot == "."}
   # end
-
 
 end
 
 new = Computer_player.new
+new.make_board
 
-new.place_piece
+new.play_piece
--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -5,6 +5,6 @@ RSpec.describe Computer_player do
   it "class exists" do
     new_cpu = Computer_player.new
     expect(new_cpu).to be_an_instance_of(Computer_player)
-
+    
   end
 end
